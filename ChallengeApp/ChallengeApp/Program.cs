068d7fdf77c14c84ad3267a29f037e7036266c66
Marek D <marek.wroc@gmail.com>
2023-04-14 19:13:50 +0200

    ZD 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Marek";
+string gender = "male"; // male, female
+int age = 40;
+if (age < 18)
+{
+    Console.Write("Niepełnoletni");
+}
+else if (age >= 18 && age < 30)
+{
+    Console.Write("Młod");
+}
+else if (age >= 30 && age < 55)
+{
+    Console.Write("Dojrzał");
+}
+else if (age >= 55)
+{
+    Console.Write("Star");
+}
+if (gender == "male")
+{
+    if (age < 18)
+    {
+        Console.Write(" mężczyzna");
+    }
+    else
+    {
+        Console.Write("y mężczyzna");
+    }
+}
+else
+{
+    Console.Write("a kobieta");
+}
+Console.WriteLine(", o imieniu " + name + ".");
+
+
+
