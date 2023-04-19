b1d407ad6532a4eebb1ffd9d2f1534c208709e8c
Marek D <marek.wroc@gmail.com>
2023-04-19 20:16:54 +0200

    ZD 11

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,11 +1,15 @@
 ﻿using ChallengeApp;
 
 var employee = new Employee("Marek", "Marecki");
-employee.AddGrade("Marek");
+employee.AddGrade(9f);
 employee.AddGrade("200");
 employee.AddGrade(3);
 employee.AddGrade(4);
 var statistics = employee.GetStatistics();
+var statistics1 = employee.GetStatisticsWithForEach();
+var statistics2 = employee.GetStatisticsWithFor();
+var statistics3 = employee.GetStatisticsWitchDoWhile();
+var statistics4 = employee.GetStatisticsWithWhile();
 Console.WriteLine($"Imie: {employee.Name}");
 Console.WriteLine($"Nazwisko: {employee.Surname}");
 Console.WriteLine($"Średnia: {statistics.Average:N2}");
